d493129cf540bf0ca823322341954936e21ef1ac
alumne <joanchorto1@iesebre.com>
2024-12-12 17:09:03 +0100

    Creacio de models , controllers, migracions, routes de RRHH

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,8 @@ Route::middleware('check.company.plan')->group(function () {
     require __DIR__ . '/web/TPV.php';
     require __DIR__ . '/web/Clients.php';
     require __DIR__ . '/web/Facturacion.php';
+//    require __DIR__ . '/web/Proyectos.php';
+    require __DIR__ . '/web/RRHH.php';
 
 
 });
