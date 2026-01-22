479d8ec90aba43cc5faefded66d7c8676d1e79d3
Joan Chorto <113586166+joanchorto1@users.noreply.github.com>
2026-01-22 12:20:26 +0100

    Refresh minimalist styling for emails and PDFs

--- resources/views/emails/team-invitation.blade.php
+++ resources/views/emails/team-invitation.blade.php
@@ -1,23 +1,25 @@
 @component('mail::message')
-{{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}
+<div style="font-family: 'Helvetica Neue', Arial, sans-serif; color: #111827; line-height: 1.6;">
+    <p style="margin: 0 0 16px;">{{ __('You have been invited to join the :team team!', ['team' => $invitation->team->name]) }}</p>
 
-@if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::registration()))
-{{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}
+    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::registration()))
+        <p style="margin: 0 0 16px;">{{ __('If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the team invitation:') }}</p>
 
-@component('mail::button', ['url' => route('login')])
-{{ __('Access JCT Agency Suite') }}
-@endcomponent
+        @component('mail::button', ['url' => route('login')])
+            {{ __('Access JCT Agency Suite') }}
+        @endcomponent
 
-{{ __('If you already have an account, you may accept this invitation by clicking the button below:') }}
+        <p style="margin: 16px 0;">{{ __('If you already have an account, you may accept this invitation by clicking the button below:') }}</p>
 
-@else
-{{ __('Access to the JCT Agency suite is managed internally. If you require credentials, please contact your JCT administrator. Once your account is active you can accept this invitation using the button below:') }}
-@endif
+    @else
+        <p style="margin: 0 0 16px;">{{ __('Access to the JCT Agency suite is managed internally. If you require credentials, please contact your JCT administrator. Once your account is active you can accept this invitation using the button below:') }}</p>
+    @endif
 
 
-@component('mail::button', ['url' => $acceptUrl])
-{{ __('Accept Invitation') }}
-@endcomponent
+    @component('mail::button', ['url' => $acceptUrl])
+        {{ __('Accept Invitation') }}
+    @endcomponent
 
-{{ __('If you did not expect to receive an invitation to this team, you may discard this email.') }}
+    <p style="margin: 16px 0 0; color: #6b7280;">{{ __('If you did not expect to receive an invitation to this team, you may discard this email.') }}</p>
+</div>
 @endcomponent
